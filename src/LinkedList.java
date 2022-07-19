16eea66821254bfb538be4c21e3a940cf936c518
Adhiyaman-Sisubalan <adhiyamansisubalan@gmail.com>
2022-07-19 21:20:42 +0800

    added addFirst,addLast and indexOf methods

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,10 +1,56 @@
+import java.util.ArrayList;
+
 public class LinkedList {
+    private class Node {
+        private int value;
+        private Node next;
+        public Node(int value) {
+            this.value = value;
+        }
+    }
     private Node first;
     private Node last;
 
-    public void addFirst(int num){
-        if(first.getValue() != 0){
-            first.setNext(num);
+    private boolean isEmpty(){
+        return first == null;
+    }
+    public void addLast(int item){
+        var node = new Node(item);
+        if(isEmpty())
+            first = last = node;
+        else{
+            last.next = node;
+            last = node;
         }
     }
+
+    public void addFirst(int item){
+        var node = new Node(item);
+        if(isEmpty())
+            first = last = node;
+        else{
+            node.next =first;
+            first = node;
+        }
+    }
+
+    public int indexOf(int item){
+        int index = 0;
+        var current = first;
+        while(current != null){
+            if(current.value == item) return index;
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+
+    /*public int[] toArray(LinkedList list){
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (var item :
+                list) {
+            arr.add(list. )
+        }
+    }*/
 }
